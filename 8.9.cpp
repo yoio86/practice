f51a9cb3c96ec376d7a1306c05f4e5a122804e7a
yoio86 <13859863923@163.com>
2026-05-08 15:17:11 +0800

    init: 本地项目初始提交

---
+++ 8.9.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+class Base {
+public:
+	virtual void fun1() {
+		cout << "Base::fun1()" << endl;
+	}
+	virtual ~Base() {}
+};
+class Derived1 : public Base {
+public:
+	virtual void fun1() {
+		cout << "Derived::fun1()" << endl;
+	};
+	virtual void fun2() {
+		cout << "Derived::fun2()" << endl;
+	}
+};
+class Derived2 : public Derived1 {
+	public:
+	virtual void fun1() {
+		cout << "Derived2::fun1()" << endl;
+	};
+	virtual void fun2() {
+		cout << "Derived2::fun2()" << endl;
+	}
+};
+void fun(Base* b) {
+	b->fun1();
+	Derived1 * d = static_cast<Derived1*>(b);
+	if (d != 0)d->fun2();
+	else
+		cout << b << "  " << d << endl;
+};
+int main() {
+	Base b;
+	fun(&b);
+	Derived1 d1;
+	fun(&d1);
+	Derived2 d2;
+	fun(&d2);
+	return 0;
+}
