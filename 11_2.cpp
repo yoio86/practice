5872a0454a7268679e51922e3a579585aff21a88
yoio86 <13859863923@163.com>
2026-05-22 16:43:51 +0800

    Add files via upload
    11_2.cpp

---
+++ 11_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        cout << "用法：" << argv[0] << " 文件名" << endl;
+        return 1;
+    }
+    ifstream inFile(argv[1], ios::binary);
+    if (!inFile)
+    {
+        cout << "文件打开失败！" << endl;
+        return 1;
+    }
+    string line;
+    int lineNum = 1;
+    while (getline(inFile, line))
+    {
+        cout << lineNum << ". " << line << endl;
+        lineNum++;
+    }
+
+    inFile.close();
+    return 0;
+}
